75f659c75997c12595e57162a150f2b43877c14d
Jason zhang <jason.zhang@freshii.com>
2018-03-09 21:45:41 -0500

    state to widget

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,19 @@ import '../App.css';
 
 class App extends Component {
 
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedWeek: null,
+    };
+    this.handleWeekClick = this.handleWeekClick.bind(this);
+  }
+
+  handleWeekClick(week, { selected }) {
+    this.setState({
+      selectedWeek: selected ? undefined : week,
+    });
+  }
 
   render() {
     return (
@@ -13,8 +25,13 @@ class App extends Component {
        <DayPicker
           firstDayOfWeek={1}
           showWeekNumbers
-          onWeekClick={(week, days) => console.log(week, days)}
+          onWeekClick={this.handleWeekClick}
         />
+        <p>
+          {this.state.selectedWeek
+            ? this.state.selectedWeek
+            : 'Please select a day 👻'}
+        </p>
         <a className="DarkSky" href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
       </div>
     );
